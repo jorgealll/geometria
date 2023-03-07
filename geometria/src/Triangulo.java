7b04f1ba6ec00db71399c71a43ef33968875c6e0
jorgealll <117639889+jorgealll@users.noreply.github.com>
2023-03-07 20:24:22 +0100

    método en una línea

--- geometria/src/Triangulo.java
+++ geometria/src/Triangulo.java
@@ -1,29 +1,26 @@
 
 public class Triangulo_JAZ extends FiguraGeometrica_JAZ{
-	private double lado1;
-	private double lado2;
-	private double lado3;
-	
-	public Triangulo(String tipoFigura, double lado1, double lado2, double lado3) {
-		super(tipoFigura);
-		lado1 = lado1;
-		lado2 = lado2;
-		lado3 = lado3;
-	}
+    private double lado1;
+    private double lado2;
+    private double lado3;
 
-	@Override
-	public double perimetro() {
-		return lado1 + lado2 + lado3;
-	}
+    public Triangulo(String tipoFigura, double lado1, double lado2, double lado3) {
+        super(tipoFigura);
+        lado1 = lado1;
+        lado2 = lado2;
+        lado3 = lado3;
+    }
 
-	@Override
-	public double area() { //Utiliza la formula de Her—n
-		double sp;
-		sp = this.semiPerimetro();
-		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
-	}
-	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
-	}
+    @Override
+    public double perimetro() {
+        return lado1 + lado2 + lado3;
+    }
+
+    @Override
+    public double area() { //Utiliza la formula de Her—n
+        double sp;
+        sp = this.perimetro() / 2;
+        return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
+    }
+
 }
